dfe5973c637f99ebf8123ccb6a8eb0e75b63368f
Felipe S Gomes <65570716+FelipeSGomes06@users.noreply.github.com>
2025-05-27 18:28:52 -0300

    Atualização
    Correção de erros e atualização de imagens

--- Menu.py
+++ Menu.py
@@ -17,7 +17,7 @@ clicou = False
 BRANCO = (255, 255, 255)
 LARANJA_ESCURO = (194, 103, 0)
 LARANJA_CLARO = (255, 178, 79)
-
+PRETO = (0, 0, 0)
 # Fonte
 fonte = pygame.font.SysFont("comicsansms", 40)
 
@@ -68,13 +68,7 @@ def jogar():
     estado = "fase1"
     senha_digitada = ""
 
-    imagens_locais = {
-        "fase": pygame.image.load("img/fase1.jpg"),
-        #"fase2": pygame.image.load("img/fase2.jpg"),
-    }
 
-    for chave in imagens_locais:
-        imagens_locais[chave] = pygame.transform.scale(imagens_locais[chave], (LARGURA, ALTURA))
 
     imagemradio = False
     imagemfolha = False
@@ -86,7 +80,12 @@ def jogar():
     imgescaravelho = False
     imgesfingecama = False
     imgesfingevazo = False
-
+    imgrio1 = False
+    imgrio2 = False
+    imgdomP = False
+    imgcavalo = False
+    imgband = False
+    imgenigma = False
 
     rodando = True
     while rodando:
@@ -110,9 +109,9 @@ def jogar():
             cenamapa = pygame.transform.scale(cenamapa,(LARGURA, ALTURA))
 
             imagemradio = ponto_interagivel(imagemradio,751, 539, 50, 50)
-            imagemfolha = ponto_interagivel(imagemfolha, 1064, 530, 40, 40)
+            imagemfolha = ponto_interagivel(imagemfolha, 1200, 600, 30, 65)
             imagemquadro = ponto_interagivel(imagemquadro,834,188,110,120)
-            imagemmapa = ponto_interagivel(imagemmapa,1200,600,30,65)
+            imagemmapa = ponto_interagivel(imagemmapa,1064,530,40,40)
             
             if imagemradio:
                 TELA.blit(cenaradio, (0,0))
@@ -125,9 +124,9 @@ def jogar():
             else:
                 TELA.blit(fase1,(0,0))
 
-        if botao_invisivel_click(48, 457, 100, 50):
-            estado = "senha"
-            senha_digitada = ""
+            if botao_invisivel_click(48, 457, 100, 50):
+                estado = "senha"
+                senha_digitada = ""
 
         if estado == 'senha':
             cenasenha = pygame.image.load('img/cenasenha.jpg')
@@ -144,35 +143,55 @@ def jogar():
                     x, y = evento.pos
                     clicou = True
 
-        # Tecla 1 (x: 450, y: 330, largura: 75, altura: 45)
+        
                     if 450 <= x <= 525 and 330 <= y <= 375:
                         if len(senha_digitada) < 4:
                             senha_digitada += "1"
 
-                    # Tecla 9 (x: 611, y: 462, largura: 75, altura: 80)
+                    
                     elif 611 <= x <= 686 and 462 <= y <= 542:
                         if len(senha_digitada) < 4:
                             senha_digitada += "9"
 
-                    # Tecla 4 (x: 450, y: 395, largura: 75, altura: 80)
+                    
                     elif 450 <= x <= 525 and 395 <= y <= 475:
                         if len(senha_digitada) < 4:
                             senha_digitada += "4"
 
-                    # Tecla 5 (x: 533, y: 395, largura: 75, altura: 80)
+                    
                     elif 533 <= x <= 608 and 395 <= y <= 475:
                         if len(senha_digitada) < 4:
                             senha_digitada += "5"
 
-                    # Confirmar (x: 711, y: 350, largura: 160, altura: 50)
+                    
                     elif 711 <= x <= 871 and 350 <= y <= 400:
                         if senha_digitada == "1945":
                             estado = "fase2"
                         else:
                 
                             senha_digitada = ""  
+
                 elif evento.type == pygame.MOUSEBUTTONUP:
                     clicou = False  
+
+                if(len(senha_digitada) == 1):
+                    pygame.draw.circle(TELA, PRETO, (423, 283), radius = 25)
+
+                elif(len(senha_digitada) == 2):
+                    pygame.draw.circle(TELA, PRETO, (423, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (523, 283), radius = 25)
+
+                elif(len(senha_digitada) == 3):
+                    pygame.draw.circle(TELA, PRETO, (423, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (523, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (623, 283), radius = 25)
+
+                elif(len(senha_digitada) == 4):
+                    pygame.draw.circle(TELA, PRETO, (423, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (523, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (623, 283), radius = 25)
+                    pygame.draw.circle(TELA, PRETO, (723, 283), radius = 25)
+
 
         if estado == 'fase2':
             imgfase2 = pygame.image.load('img/imgfase2.jpg')
@@ -199,6 +218,11 @@ def jogar():
             imgescaravelho = ponto_interagivel(imgescaravelho, 451, 441, 15, 35)
             imgesfingevazo = ponto_interagivel(imgesfingevazo, 1090, 620, 80, 70)
             imgesfingecama = ponto_interagivel(imgesfingecama, 308, 504 , 52, 86)
+            if (botao_invisivel_click(20, 438, 50, 22)):
+                estado = 'senha2'
+                senha_digitada = ''
+                fonte = pygame.font.Font(None, 48)
+                texto_digitado = ""
 
             if(imgpapiro):
                 TELA.blit(cenapapiro, (0,0))
@@ -216,12 +240,85 @@ def jogar():
                 TELA.blit(imgfase2, (0,0))
 
 
-        elif estado in imagens_locais:
-            TELA.blit(imagens_locais[estado], (0, 0))
+        if(estado == 'senha2'):
+            cenasenha2 = pygame.image.load('img/cenasenha2.jpg')
+            cenasenha2 = pygame.transform.scale(cenasenha2, (LARGURA, ALTURA))
+            for evento in pygame.event.get():
+                 if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_BACKSPACE:
+                            texto_digitado = texto_digitado[:-1]
+                    else:
+                            texto_digitado += evento.unicode
+
+            
+            if (botao_invisivel_click(767,367, 160, 50)):
+                estado = 'fase2'
+
+
+            if(botao_invisivel_click(408, 367, 160, 50)):
+                if(texto_digitado.upper() == 'ESFINGE DE GIZE' or texto_digitado.upper() == 'ESFINGE DE GIZÉ'):
+
+                    estado = 'fase3'
+            print(texto_digitado)
+
+
+        
+            superficie_texto = fonte.render(texto_digitado, True, (0, 0, 0)) 
+            TELA.blit(cenasenha2, (0 , 0))
+            TELA.blit(superficie_texto, (425, 299))
+        
+
+        if(estado == 'fase3'):
+            imgfase3 = pygame.image.load('img/fase3.jpg')
+            imgfase3 = pygame.transform.scale(imgfase3, (LARGURA, ALTURA))
+
+            cenario1 = pygame.image.load('img/cena_rio1.jpg')
+            cenario1 = pygame.transform.scale(cenario1, (LARGURA, ALTURA))
+            cenario2 = pygame.image.load('img/cena_rio2.jpg')
+            cenario2 = pygame.transform.scale(cenario2, (LARGURA, ALTURA))
+            cenadomP = pygame.image.load('img/cenadompedro.jpg')
+            cenadomP = pygame.transform.scale(cenadomP, (LARGURA, ALTURA))
+            cenaenigma2 = pygame.image.load('img/cenaenigma2.jpg')
+            cenaenigma2 = pygame.transform.scale(cenaenigma2, (LARGURA, ALTURA))
+            cenaband = pygame.image.load('img/cenaband.jpg')
+            cenaband = pygame.transform.scale(cenaband, (LARGURA, ALTURA))
+            cenacavalo = pygame.image.load('img/cenacavalo.jpg')
+            cenacavalo = pygame.transform.scale(cenacavalo, (LARGURA, ALTURA))
+            
+            imgrio1 = ponto_interagivel(imgrio1, 895, 96, 50, 56)
+            imgrio2 = ponto_interagivel(imgrio2, 956, 126, 70, 80)
+            imgdomP = ponto_interagivel(imgdomP, 447, 52, 120, 150)
+            imgenigma = ponto_interagivel(imgenigma, 523, 421 ,13, 30)
+            imgband = ponto_interagivel(imgband, 874, 336, 35, 42)
+            imgcavalo = ponto_interagivel(imgcavalo, 330, 193, 30, 30)
+
+            
+            if(imgrio1):
+                TELA.blit(cenario1, (0,0))
+
+            elif(imgrio2):
+                TELA.blit(cenario2, (0,0))
+
+            elif(imgdomP):
+                TELA.blit(cenadomP, (0,0))
+
+            elif(imgenigma):
+                TELA.blit(cenaenigma2, (0,0))
+
+            elif(imgband):
+                TELA.blit(cenaband, (0,0))
+
+            elif(imgcavalo):
+                TELA.blit(cenacavalo, (0,0))
+
+            else:
+                TELA.blit(imgfase3, (0,0))
+
 
         pygame.display.update()
-        print(senha_digitada)
-        print(pygame.mouse.get_pos())
+        print(estado)
+    
+        
 
 def creditos():
     print("Autores do projeto...")
